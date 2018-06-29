cb1e9f414194052e88b4b6eea93918275335161e
Yomguithereal <guillaumeplique@gmail.com>
2018-06-29 18:08:56 +0200

    Modernization

--- build/graphology-layout-forceatlas2.js
+++ build/graphology-layout-forceatlas2.js
@@ -7,7 +7,7 @@
 		exports["forceAtlas2"] = factory();
 	else
 		root["forceAtlas2"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};

3f29b83b4804194e5bc5fbda9e63071a02a646f9
Yomguithereal <guillaumeplique@gmail.com>
2020-04-07 20:17:56 +0200

    Fixing profile...

--- benchmark/profile.js
+++ benchmark/profile.js
@@ -1,15 +1,17 @@
 var Graph = require('graphology');
 var generateClusters = require('graphology-generators/random/clusters');
+var randomLayout = require('graphology-layout/random');
 var layout = require('../');
 
-var ORDER = 5000;
-var SIZE = 10000;
+var ORDER = 50000;
+var SIZE = 100000;
 var CLUSTERS = 10;
 
 var graph = generateClusters(Graph.UndirectedGraph, {order: ORDER, size: SIZE, clusters: CLUSTERS});
+randomLayout.assign(graph);
 
 console.time('layout');
 console.profile('layout');
-layout(graph, {iterations: 1, settings: {barnesHutOptimize: true}});
+layout(graph, {iterations: 5, settings: {barnesHutOptimize: true}});
 console.profileEnd('layout');
 console.timeEnd('layout');

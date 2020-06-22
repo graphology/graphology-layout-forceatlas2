7faf5c63937fbcc8496ac929189deef9f9a48459
Yomguithereal <guillaumeplique@gmail.com>
2020-06-22 18:06:21 +0200

    Relaxed conf types

--- index.d.ts
+++ index.d.ts
@@ -3,21 +3,21 @@ import Graph from 'graphology-types';
 type LayoutMapping = {[key: string]: {x: number, y: number}};
 
 export type ForceAtlas2Settings = {
-  linLogMode: boolean,
-  outboundAttractionDistribution: boolean,
-  adjustSizes: boolean,
-  edgeWeightInfluence: number,
-  scalingRatio: number,
-  strongGravityMode: boolean,
-  gravity: number,
-  slowDown: number,
-  barnesHutOptimize: boolean,
-  barnesHutTheta: number
+  linLogMode?: boolean,
+  outboundAttractionDistribution?: boolean,
+  adjustSizes?: boolean,
+  edgeWeightInfluence?: number,
+  scalingRatio?: number,
+  strongGravityMode?: boolean,
+  gravity?: number,
+  slowDown?: number,
+  barnesHutOptimize?: boolean,
+  barnesHutTheta?: number
 };
 
 export type ForceAtlas2LayoutOptions = {
   iterations: number,
-  settings: ForceAtlas2Settings
+  settings?: ForceAtlas2Settings
 };
 
 interface IForceAtlas2Layout {
